e3a460ef38ca764f42cf66d0e1073c99e71d1428
Carlos Takata <carlos.takata@gmail.com>
2021-10-29 15:43:21 -0300

    Added delete API

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Header, HttpCode, Post, Req, Res } from "@nestjs/common";
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, Header, HttpCode, Param, Post, Req, Res } from "@nestjs/common";
+import { ApiBody, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Product } from "src/interface/product.interface";
 import { ProductService } from "./product.service";
 import { Response, Request } from 'express';
@@ -24,6 +24,53 @@ export class ProductController {
     } catch(e) {
       console.log(e.stack)
       throw new Error('Cannot save product')
+    }
+  }
+
+  @Get('/all')
+  @ApiOperation({ summary: 'Retorna todos os produtos' })
+  @ApiResponse( { status: HTTP_CODE.OK, description: 'Retorna lista de produtos salvos' })
+  @HttpCode(HTTP_CODE.OK)
+  @Header('Content-type', 'application/json')
+  async listProducts(@Res() res: Response, @Req() req: Request): Promise<Response> {
+    try {
+      const savedProduct = await this.productService.findAll()
+      return res.status(savedProduct.status).json(savedProduct.response)
+    } catch(e) {
+      console.log(e.stack)
+      throw new Error('Cannot list products')
+    }
+  }
+
+  @Get('/get/:id')
+  @ApiOperation({ summary: 'Retorna um produto pelo id' })
+  @ApiResponse( { status: HTTP_CODE.OK, description: 'Retorna produto' })
+  @ApiParam({ name: 'id' })
+  @HttpCode(HTTP_CODE.OK)
+  @Header('Content-type', 'application/json')
+  async getProduct(@Res() res: Response, @Req() req: Request, @Param() params): Promise<Response> {
+    try {
+      const savedProduct = await this.productService.get(params.id)
+      return res.status(savedProduct.status).json(savedProduct.response)
+    } catch(e) {
+      console.log(e.stack)
+      throw new Error('Cannot get product')
+    }
+  }
+
+  @Delete('/delete/:id')
+  @ApiOperation({ summary: 'Apaga um produto pelo id' })
+  @ApiResponse( { status: HTTP_CODE.OK, description: 'Retorna sucesso' })
+  @ApiParam({ name: 'id' })
+  @HttpCode(HTTP_CODE.OK)
+  @Header('Content-type', 'application/json')
+  async deleteProduct(@Res() res: Response, @Req() req: Request, @Param() params): Promise<Response> {
+    try {
+      const savedProduct = await this.productService.delete(params.id)
+      return res.status(savedProduct.status).json(savedProduct.response)
+    } catch(e) {
+      console.log(e.stack)
+      throw new Error('Cannot get product')
     }
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from "@nestjs/common";
 import { Model } from "mongoose";
 import { HTTP_CODE } from "src/errorcodes/error";
 import { Product } from "../interface/product.interface";
+import * as mongoose from "mongoose";
 
 @Injectable()
 export class ProductService {
@@ -11,14 +12,25 @@ export class ProductService {
         private productModel: Model<Product>
     ) {}
 
-    public async save(productDto: Product): Promise<{status: number, response: any}> {
+    public async save(productDto: Product): Promise<{status: number, response: Product}> {
         const createProduct = new this.productModel(productDto);
         const product = await createProduct.save();
-        console.log(product)
         return { status: HTTP_CODE.OK, response: product }
     }
 
-    public async findAll(): Promise<Product[]> {
-        return this.productModel.find().exec();
+    public async findAll(): Promise<{status: number, response: Product[]}> {
+        const products = await this.productModel.find().exec();
+        return { status: HTTP_CODE.OK, response: products }
     }
+
+    public async get(id: string): Promise<{status: number, response: Product}> {
+        const product = await this.productModel.findById(new mongoose.Types.ObjectId(id)).exec();
+        return { status: HTTP_CODE.OK, response: product }
+    }
+
+    public async delete(id: string): Promise<{status: number, response: boolean}> {
+        await this.productModel.deleteOne({ _id: id }).exec();
+        return { status: HTTP_CODE.OK, response: true }
+    }
+
 }
